5e90f7c1e0c261631dbf9b07d655c1438b4516e3
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-04-19 13:58:15 +0530

    Add ApplyJob page with job details display; integrate Loader, Navbar, and Footer components; include k-convert and moment for salary conversion and date formatting

--- job-board/client/src/components/NavBar.jsx
+++ job-board/client/src/components/NavBar.jsx
@@ -1,22 +1,30 @@
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { assets } from "../assets/assets";
-import {Link} from 'react-router'
+import { Link, useNavigate } from "react-router";
 
 const NavBar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
+  const navigate = useNavigate();
 
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
-        <img src={assets.logo} alt="" />
+        <img
+          onClick={() => navigate("/")}
+          className="cursor-pointer"
+          src={assets.logo}
+          alt=""
+        />
         {user ? (
           <div className="flex items-center gap-3">
             <button className="bg-blue-500 text-slate-50 rounded-full px-3 py-2">
               <Link to={"/applications"}>Applied Jobs</Link>
             </button>
             <p className="font-light"> | </p>
-            <p className="font-light max-sm:hidden">Hi, {user.firstName + " " + user.lastName}</p>
+            <p className="font-light max-sm:hidden">
+              Hi, {user.firstName + " " + user.lastName}
+            </p>
             <UserButton />
           </div>
         ) : (
