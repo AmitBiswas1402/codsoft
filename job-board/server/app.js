65c55b2a5f41a79d7786cdabf4ce98fda43352a6
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-04-23 12:20:02 +0530

    chore: initialize server package.json with dependencies and scripts

---
+++ job-board/server/app.js
@@ -0,0 +1,31 @@
+import "./config/instrument.js";
+import * as Sentry from "@sentry/node";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectToDB from "./db/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Job Board API");
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectToDB();
+});
+
+export default app;
+
